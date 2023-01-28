3959046faec914e2a1956162225437095e61c07b
LiTao <1287862489@qq.com>
2023-01-28 14:32:38 +0800

    feat:新增JSON解析和验证器封装

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -2,52 +2,38 @@
 package requests
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/thedevsaddam/govalidator"
+    "github.com/gin-gonic/gin"
+    "github.com/thedevsaddam/govalidator"
 )
 
 type SignupPhoneExistRequest struct {
-	Phone string `json:"phone,omitempty" valid:"phone"`
+    Phone string `json:"phone,omitempty" valid:"phone"`
 }
 
-func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
-
-	// 自定义验证规则
-	rules := govalidator.MapData{
-		"phone": []string{"required", "digits:11"},
-	}
-
-	// 自定义验证出错时的提示
-	messages := govalidator.MapData{
-		"phone": []string{
-			"required:手机号为必填项，参数名称 phone",
-			"digits:手机号长度必须为 11 位的数字",
-		},
-	}
-
-	// 配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-		Messages:      messages,
-	}
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+func SignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
+    // 自定义验证规则
+    rules := govalidator.MapData{
+        "phone": []string{"required", "digits:11"},
+    }
+    // 自定义验证出错时的提示
+    messages := govalidator.MapData{
+        "phone": []string{
+            "required:手机号为必填项，参数名称 phone",
+            "digits:手机号长度必须为 11 位的数字",
+        },
+    }
+    return validate(data, rules, messages)
 }
 
 type SignupEmailExistRequest struct {
     Email string `json:"email,omitempty" valid:"email"`
 }
 
-func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
-
+func SignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
     // 自定义验证规则
     rules := govalidator.MapData{
         "email": []string{"required", "min:4", "max:30", "email"},
     }
-
     // 自定义验证出错时的提示
     messages := govalidator.MapData{
         "email": []string{
@@ -57,15 +43,5 @@ func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]str
             "email:Email 格式不正确，请提供有效的邮箱地址",
         },
     }
-
-    // 配置初始化
-    opts := govalidator.Options{
-        Data:          data,
-        Rules:         rules,
-        TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-        Messages:      messages,
-    }
-
-    // 开始验证
-    return govalidator.New(opts).ValidateStruct()
+    return validate(data, rules, messages)
 }
